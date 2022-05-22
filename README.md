794c3d982e8f3932cf41713fa3c551006e35e422
kishorekcs <kcs.kishore1997@gmail.com>
2022-05-22 14:23:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-my-float-label-exp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wv4ojr)
---
+++ src/library/my-float-label/my-float-label.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-float-label',
+  templateUrl: './my-float-label.component.html',
+  styleUrls: ['./my-float-label.component.scss'],
+})
+export class MyFloatLabelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
